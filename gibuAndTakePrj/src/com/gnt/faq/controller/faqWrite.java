b42972914c9cfa9ad533260e1797ef03ef01e888
DESKTOP-AJ76NJ9\pcs1214 <acs>
2022-08-16 21:45:06 +0900

    서블릿 경로 수정

---
+++ gibuAndTakePrj/src/com/gnt/faq/controller/faqWrite.java
@@ -0,0 +1,18 @@
+package com.gnt.faq.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/faq/write1")
+public class faqWrite extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/views/FAQ/faqWrite.jsp").forward(req, resp);
+	}
+}
