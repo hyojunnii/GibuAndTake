f03ab049e0dd115d4876846544a8a8efaca1b73f
DESKTOP-AJ76NJ9\pcs1214 <acs>
2022-08-20 23:56:34 +0900

    공지사항 부분 수정

--- gibuAndTakePrj/src/com/gnt/notice/controller/NoticeDelete.java
+++ gibuAndTakePrj/src/com/gnt/notice/controller/NoticeDelete.java
@@ -8,11 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gnt.notice.service.NoticeService;
+
 @WebServlet(urlPatterns = "/notice/delete")
 public class NoticeDelete extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String num = req.getParameter("num");
+		
+		//int result = new NoticeService().delete(no);
+		
+	//	if(result == 1) {
+			resp.sendRedirect("/gibuAndTakePrj/notice/list");
+		//}
 		
 	}
 }
